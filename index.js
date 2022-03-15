4ac6baf3e099666004b6a5f54af49b586b03b526
Jodie Solomons <jodies383@gmail.com>
2022-03-15 14:27:41 +0200

    highlights words

---
+++ index.js
@@ -0,0 +1,18 @@
+const analyzeSentenceBtn = document.querySelector(".analyzeSentence")
+let enterSentence = document.querySelector(".enterSentence")
+let displaySentence = document.querySelector(".displaySentence")
+let displayLength = document.querySelector(".displayLength")
+
+analyzeSentenceBtn.addEventListener('click', () => {
+    let enter = enterSentence.value
+    let split = enter.split(" ")
+   
+    const highlightedWords = split.map(word => {
+        if (word.length > 4) {
+            return `<mark>${word}<mark>`
+        }
+    })
+ 
+    displaySentence.innerHTML = highlightedWords;
+    displayLength.innerHTML = split.length;
+})
