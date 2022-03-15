b77b36ce75612fa476d50149c8af7a935cd6a3b4
Jodie Solomons <jodies383@gmail.com>
2022-03-15 15:28:59 +0200

    highlighted words fixed

--- index.js
+++ index.js
@@ -6,13 +6,21 @@ let displayLength = document.querySelector(".displayLength")
 analyzeSentenceBtn.addEventListener('click', () => {
     let enter = enterSentence.value
     let split = enter.split(" ")
-   
+    let sentence = ""
+    // let list = []
+    // list.push(enter)
     const highlightedWords = split.map(word => {
         if (word.length > 4) {
-            return (`<mark>${word}<mark>`)
+            return `<mark>${word}</mark>`
         }
+        return word
     })
- 
-    displaySentence.innerHTML = highlightedWords;
+    for (let index = 0; index < highlightedWords.length; index++) {
+        sentence += highlightedWords[index] + " ";
+    }
+    // console.log(highlightedWords);
+    // console.log(sentence);
+    displaySentence.innerHTML = sentence;
     displayLength.innerHTML = `there are ${split.length} words in your sentence`;
+    console.log(displaySentence.innerHTML);
 })
