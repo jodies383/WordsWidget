0a6b3847fb9535ea16c0448b1ea9d1607173b847
Jodie Solomons <jodies383@gmail.com>
2022-03-16 15:48:24 +0200

    highlighting the longest word

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const hideUnderFive = document.querySelector(".hideUnderFive")
 let enterSentence = document.querySelector(".enterSentence")
 let displaySentence = document.querySelector(".displaySentence")
 let displayLength = document.querySelector(".displayLength")
+let longestWordDisplay = document.querySelector(".longestWordDisplay")
+
 
 analyzeSentenceBtn.addEventListener('click', () => {
     let enter = enterSentence.value
@@ -18,24 +20,24 @@ analyzeSentenceBtn.addEventListener('click', () => {
             return sortedWord[0];
         }
         const highlightedWords = split.map(word => {
+            
             if (word.length > 4) {
                 return `<mark>${word}</mark>`
             }
             return word
+            
         })
         for (let index = 0; index < highlightedWords.length; index++) {
             sentence += highlightedWords[index] + " ";
         }
-        for (let index = 0; index < longestWord().length; index++) {
-            sentence += longestWord()[index] + " ";
-        }
         displaySentence.innerHTML = sentence;
         displayLength.innerHTML = `there are ${split.length} words in your sentence`;
+        longestWordDisplay.innerHTML = `the longest word is <mark2>${longestWord()}</mark2>`
     }
     else {
         displaySentence.innerHTML = "no sentence found";
     }
-    enter = ""
+    
 })
 
 
@@ -69,8 +71,6 @@ hideUnderFive.addEventListener('click', () => {
             for (let index = 0; index < highlightedWords.length; index++) {
                 sentence += highlightedWords[index] + " ";
             }
-
-
             displaySentence.innerHTML = sentence;
             displayLength.innerHTML = `there are ${split.length} words in your sentence`;
         }
