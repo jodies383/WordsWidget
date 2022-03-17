278aae6b6535a7b1eae76d83fb0d0d5439de5719
Jodie Solomons <jodies383@gmail.com>
2022-03-17 16:46:09 +0200

    getting both highlighted words

--- index.js
+++ index.js
@@ -1,52 +1,68 @@
 const analyzeSentenceBtn = document.querySelector(".analyzeSentence")
 const hideUnderFive = document.querySelector(".hideUnderFive")
+const template = document.querySelector(".wordsWidget").innerHTML
+const wordsTemplate = Handlebars.compile(template)
 let enterSentence = document.querySelector(".enterSentence")
 let displaySentence = document.querySelector(".displaySentence")
 let displayLength = document.querySelector(".displayLength")
 let longestWordDisplay = document.querySelector(".longestWordDisplay")
+let listOfSentences = document.querySelector(".listOfSentences")
+let list = []
 
+let displayWords = wordsTemplate({
+    sentence: list
+});
 
 analyzeSentenceBtn.addEventListener('click', () => {
     let enter = enterSentence.value
+    let longWordArr = []
     let replace = enter.replace(/[^\w\s]|_/g, "")
         .replace(/\s+/g, " ");
     let split = replace.split(" ")
     let sentence = ""
-    let list = []
     if (enter) {
-        const longestWord = () => {
-          
-            //limit to last 5
-            //arr.slice(Math.max(arr.length - 5, 1))
-            let sortedWord = split.sort((shortest, longest) => {
-                return longest.length - shortest.length;
-            })
-            return sortedWord[0];
-        }
-        const highlightedWords = split.map(word => {
 
-            if (word.length > 4) {
+        let string = ""
+
+        var longestWord = 0;
+        for (var i = 0; i < split.length; i++) {
+            if (split[i].length > longestWord) {
+                longestWord = split[i].length;
+                string = split[i]
+            }
+        }
+
+
+        const highlightedWords = split.map(word => {
+            const filWord = split.filter((words) => {
+                return words.length == string.length;
+              });
+              console.log(filWord);
+            if (word.includes(filWord)) {
+                return `<mark2>${word}</mark2>`
+            }
+            else if (!word.includes(filWord) && word.length > 4) {
                 return `<mark>${word}</mark>`
             }
             return word
-
         })
-        for (let index = 0; index < highlightedWords.length; index++) {
-            sentence += highlightedWords[index] + " ";
+        for (let i = 0; i < highlightedWords.length; i++) {
+            sentence += highlightedWords[i] + " ";
         }
         let theWordCount = `there are ${split.length} words in your sentence`
-        let longestWordCount = `the longest word is <mark2>${longestWord()}</mark2>`
-        list.push({sentences: sentence, wordCount: theWordCount, longWord: longestWordCount})
-        console.log(list);
+        // let longestWordCount = `the longest word is <mark2>${longWordArr}</mark2>`
+        list.push({ sentences: sentence, wordCount: theWordCount })
+        // console.log(displayWords);
         displaySentence.innerHTML = sentence;
         displayLength.innerHTML = theWordCount;
-        longestWordDisplay.innerHTML = longestWordCount;
+        // longestWordDisplay.innerHTML = longestWordCount;
     }
     else {
         displaySentence.innerHTML = "no sentence found";
         displayLength.innerHTML = "";
         longestWordDisplay.innerHTML = "";
     }
+    listOfSentences.innerHTML = displayWords
 
 })
 
@@ -95,4 +111,5 @@ hideUnderFive.addEventListener('click', () => {
 
     }
 });
-
+      //limit to last 5
+  //arr.slice(Math.max(arr.length - 5, 1))
