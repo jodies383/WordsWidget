d0a4c427fa2146067cc7c3d097c1ca655459313e
Jodie Solomons <jodies383@gmail.com>
2022-03-18 16:56:30 +0200

    added last five sentences

--- index.js
+++ index.js
@@ -7,20 +7,23 @@ let displaySentence = document.querySelector(".displaySentence")
 let displayLength = document.querySelector(".displayLength")
 let longestWordDisplay = document.querySelector(".longestWordDisplay")
 let listOfSentences = document.querySelector(".listOfSentences")
-let list = []
+let sentenceList = []
+if (localStorage['sentences']) {
+    sentenceList = JSON.parse(localStorage.getItem('sentences'));
 
+}
 let displayWords = wordsTemplate({
-    sentence: list
+    sentence: sentenceList.slice(0).slice(-5)
 });
 
-analyzeSentenceBtn.addEventListener('click', () => {
+function myFunction(evt) {
+    enterSentence.value = evt.target.innerHTML
     let enter = enterSentence.value
-    let longWordArr = []
     let replace = enter.replace(/[^\w\s]|_/g, "")
         .replace(/\s+/g, " ");
     let split = replace.split(" ")
     let sentence = ""
-    if (enter) {
+    if (enter && split.length >= 5) {
 
         let string = ""
 
@@ -31,7 +34,6 @@ analyzeSentenceBtn.addEventListener('click', () => {
                 string = split[i]
             }
         }
-
 
         const highlightedWords = split.map(word => {
             let filWord = split.filter((words) => {
@@ -48,24 +50,76 @@ analyzeSentenceBtn.addEventListener('click', () => {
         for (let i = 0; i < highlightedWords.length; i++) {
             sentence += highlightedWords[i] + " ";
         }
-        let theWordCount = `there are ${split.length} words in your sentence`
-        // let longestWordCount = `the longest word is <mark2>${longWordArr}</mark2>`
-        list.push({ sentences: sentence, wordCount: theWordCount })
-        // console.log(displayWords);
+        let theWordCount = `there are ${split.length} words in this sentence`   
         displaySentence.innerHTML = sentence;
         displayLength.innerHTML = theWordCount;
-        // longestWordDisplay.innerHTML = longestWordCount;
     }
     else {
         displaySentence.innerHTML = "no sentence found";
         displayLength.innerHTML = "";
         longestWordDisplay.innerHTML = "";
     }
+    let displayWords = wordsTemplate({
+        sentence: sentenceList.slice(0).slice(-5)
+    });
+    listOfSentences.innerHTML = displayWords
+}
+listOfSentences.addEventListener('click', myFunction)
+  
+analyzeSentenceBtn.addEventListener('click', () => {
+    if (localStorage['sentences']) {
+        sentenceList = JSON.parse(localStorage.getItem('sentences'));
+    
+    }
+    let enter = enterSentence.value
+    let replace = enter.replace(/[^\w\s]|_/g, "")
+        .replace(/\s+/g, " ");
+    let split = replace.split(" ")
+    let sentence = ""
+    if (enter && split.length >= 5) {
+
+        let string = ""
+
+        var longestWord = 0;
+        for (var i = 0; i < split.length; i++) {
+            if (split[i].length > longestWord) {
+                longestWord = split[i].length;
+                string = split[i]
+            }
+        }
+
+        const highlightedWords = split.map(word => {
+            let filWord = split.filter((words) => {
+                return words.length == string.length;
+            });
+            if (filWord.includes(word)) {
+                return `<mark2>${word}</mark2>`
+            }
+            else if (!filWord.includes(word) && word.length > 4) {
+                return `<mark>${word}</mark>`
+            }
+            return word
+        })
+        for (let i = 0; i < highlightedWords.length; i++) {
+            sentence += highlightedWords[i] + " ";
+        }
+        let theWordCount = `there are ${split.length} words in this sentence`
+        sentenceList.push(enter)
+        localStorage.setItem('sentences', JSON.stringify(sentenceList));
+        displaySentence.innerHTML = sentence;
+        displayLength.innerHTML = theWordCount;
+    }
+    else {
+        displaySentence.innerHTML = "no sentence found";
+        displayLength.innerHTML = "";
+        longestWordDisplay.innerHTML = "";
+    }
+    let displayWords = wordsTemplate({
+        sentence: sentenceList.slice(0).slice(-5)
+    });
     listOfSentences.innerHTML = displayWords
 
 })
-
-console.log(displayWords);
 
 hideUnderFive.addEventListener('click', () => {
     let enter = enterSentence.value
@@ -82,7 +136,7 @@ hideUnderFive.addEventListener('click', () => {
             string = split[i]
         }
     }
-    if (enter) {
+    if (enter && split.length >= 5) {
         if (hideUnderFive.checked === true) {
             const highlightedWords = split.map(word => {
                 let filWord = split.filter((words) => {
@@ -130,5 +184,7 @@ hideUnderFive.addEventListener('click', () => {
 
     }
 });
+listOfSentences.innerHTML = displayWords
+
       //limit to last 5
   //arr.slice(Math.max(arr.length - 5, 1))
